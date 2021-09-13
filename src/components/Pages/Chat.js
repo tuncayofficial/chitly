61d95ec792634d3d0a4d1cb02a5612379523a306
henryiscoder <qtuncay70@gmail.com>
2021-09-13 19:59:07 +0300

    Chat reset

--- src/components/Pages/Chat.js
+++ src/components/Pages/Chat.js
@@ -4,12 +4,7 @@ import { ChatEngine } from 'react-chat-engine';
 
 function Chat(props){
     return (
-        <ChatEngine
-			height='100vh'
-			userName='Tuncay'
-			userSecret='12345'
-			projectID='64caeeb3-0306-4168-aeb7-0fdc77966db1'
-		/>
+        <h1>Chat!</h1>
     )
 }
 
